72e9789e43519f32d8dcc2fba5bb1d110610de10
Sebastian Castro <4603398+sea-bass@users.noreply.github.com>
2023-03-25 16:49:20 -0400

    Add joint limits to Panda demo launch file + make consistent with tutorials (#165)

--- panda_moveit_config/launch/demo.launch.py
+++ panda_moveit_config/launch/demo.launch.py
@@ -1,10 +1,10 @@
 import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.conditions import IfCondition
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
+from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
 from moveit_configs_utils import MoveItConfigsBuilder
 
@@ -54,39 +54,31 @@ def generate_launch_description():
     )
 
     # RViz
-    tutorial_mode = LaunchConfiguration("rviz_tutorial")
-    rviz_base = os.path.join(
-        get_package_share_directory("moveit_resources_panda_moveit_config"), "launch"
+    rviz_config_arg = DeclareLaunchArgument(
+        "rviz_config",
+        default_value="moveit.rviz",
+        description="RViz configuration file",
     )
-    rviz_full_config = os.path.join(rviz_base, "moveit.rviz")
-    rviz_empty_config = os.path.join(rviz_base, "moveit_empty.rviz")
-    rviz_node_tutorial = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_empty_config],
-        parameters=[
-            moveit_config.robot_description,
-            moveit_config.robot_description_semantic,
-            moveit_config.planning_pipelines,
-            moveit_config.robot_description_kinematics,
-        ],
-        condition=IfCondition(tutorial_mode),
+    rviz_config = PathJoinSubstitution(
+        [
+            FindPackageShare("moveit_resources_panda_moveit_config"),
+            "launch",
+            LaunchConfiguration("rviz_config"),
+        ]
     )
     rviz_node = Node(
         package="rviz2",
         executable="rviz2",
         name="rviz2",
         output="log",
-        arguments=["-d", rviz_full_config],
+        arguments=["-d", rviz_config],
         parameters=[
             moveit_config.robot_description,
             moveit_config.robot_description_semantic,
-            moveit_config.planning_pipelines,
             moveit_config.robot_description_kinematics,
+            moveit_config.planning_pipelines,
+            moveit_config.joint_limits,
         ],
-        condition=UnlessCondition(tutorial_mode),
     )
 
     # Static TF
@@ -158,11 +150,10 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
-            tutorial_arg,
             db_arg,
             ros2_control_hardware_type,
+            rviz_config_arg,
             rviz_node,
-            rviz_node_tutorial,
             static_tf_node,
             robot_state_publisher,
             move_group_node,
